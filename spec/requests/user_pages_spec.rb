7271aa02613e3c97627f4ca0741df3a3c1e3fe6d
Alberto Trindade <att@cin.ufpe.br>
2014-04-16 01:41:28 -0500

    Finish Homework 10 - Exercise 4

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -69,6 +69,21 @@ describe "User pages" do
     end
   end
 
+  describe "profile page visited by other users" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:another_user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    
+    before do  
+      sign_in another_user
+      visit user_path(user)
+    end
+
+    it "should not show delete link" do
+      expect(page).not_to have_link('delete')
+    end
+  end
+
   describe "signup page" do
     before { visit signup_path }
 
